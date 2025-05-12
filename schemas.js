b00ece47a3a6a9c4bb65a049fb57f2a6d116bb3a
Homagni <2333622@cognizant.com>
2025-05-12 13:04:18 +0530

    Routes made

--- schemas.js
+++ schemas.js
@@ -12,7 +12,7 @@ module.exports.campgroundSchema = Joi.object({
 });
 
 module.exports.reviewSchema = Joi.object({ 
-    reviews: Joi.object({
+    review: Joi.object({
         rating: Joi.number().required().min(1).max(5), 
         body: Joi.string().required()
     }).required()
