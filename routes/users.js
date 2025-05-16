d60b31fef55318c1909a49e0861b39c6f9497a6a
Homagni <2333622@cognizant.com>
2025-05-16 14:13:21 +0530

    basic authentication

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const User = require('../models/user'); 
 const catchAsync = require('../utils/CatchAsync');
+const { isLoggedIn, storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -13,7 +14,11 @@ router.post('/register', catchAsync(async (req, res) => {
         const {username, email, password} = req.body;
         const user = new User({username, email});
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to YelpCamp');
+        req.login(registeredUser, err=> {
+            if(err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp');
+            res.redirect('/campgrounds');
+        })
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('/register');
@@ -24,9 +29,17 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
     req.flash('success', 'Welcome!'); 
-    res.redirect('/campgrounds');
+    res.redirect(res.locals.returnTo);
 })
+
+router.get('/logout', isLoggedIn, (req, res, next) => {
+    req.logout( err => {
+        if (err) return next(err);
+        req.flash('success', 'Logged out successfully');
+        res.redirect('/campgrounds');
+    });
+}); 
 
 module.exports = router;
